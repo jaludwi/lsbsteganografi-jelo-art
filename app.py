2a5f48304b6b7856f53433b437299731ddf0f118
jaludwi <163502966+jaludwi@users.noreply.github.com>
2024-08-12 13:05:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,29 @@
-import streamlit as st 
+import streamlit as st 
 from PIL import Image
 import numpy as np
 from dec import decryptPage
 from enc import encryptPage
-from pdf2image import convert_from_path  
 
 st.set_page_config(page_title="Jelo Art Studio", page_icon="🧐:", layout="wide")
 
 # Set up the Streamlit app
 st.title('Jelo Art Studio')
-st.header('LSB Steganografi 🧐')
+st.header('Steganografi LSB 🧐')
 
 st.write("---")
 
 # Define tab content functions
 def encrypt_tab():
-  encryptPage()
+    encryptPage()
 
 def decrypt_tab():
-  decryptPage()
+    decryptPage()
 
 # Create tabs
 tabs = ["Enkripsi", "Dekripsi"]
-selected_tab = st.radio("Jelo Art Studio", tabs)
+selected_tab = st.radio("Pilih Opsi", tabs)
 
 if selected_tab == "Enkripsi":
-  encrypt_tab()
+    encrypt_tab()
 else:
-  decrypt_tab()
+    decrypt_tab()
